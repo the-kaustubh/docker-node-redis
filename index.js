6de7e7f94e0a8e5bfcaef0ce9d94b54b49139e1f
Kaustubh Murumkar <kaustubhm@mkcl.org>
2021-10-08 18:12:09 +0530

    Mongo add trial successful

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
+  useUnifiedTopology: true
 })
 
 const db = mongoose.connection
